8e3f2420e147ceb94dcbdc2ae84a0e9a5356e43f
hp <47206353@qq.com>
2015-01-19 22:12:45 +0800

    dd

---
+++ dubbo/src/main/java/com/ws/ami/dubbo/DubboComsumeDemo.java
@@ -0,0 +1,19 @@
+package com.ws.ami.dubbo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by hp on 15-1-19.
+ */
+public class DubboComsumeDemo {
+    public static void main(String[] args) throws InterruptedException {
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext(
+                new String[] {"conf/comsumer.xml"});
+
+        DemoService demoService = (DemoService) factory.getBean("demoService");
+        demoService.sayHello();
+
+    }
+}
