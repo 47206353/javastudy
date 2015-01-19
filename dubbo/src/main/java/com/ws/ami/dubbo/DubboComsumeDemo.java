825ddfa409d88bc5c30abb9998e222aec31cf3a5
hp <47206353@qq.com>
2015-01-19 22:34:15 +0800

    dd

--- dubbo/src/main/java/com/ws/ami/dubbo/DubboComsumeDemo.java
+++ dubbo/src/main/java/com/ws/ami/dubbo/DubboComsumeDemo.java
@@ -10,7 +10,7 @@ public class DubboComsumeDemo {
     public static void main(String[] args) throws InterruptedException {
 
         ApplicationContext factory = new ClassPathXmlApplicationContext(
-                new String[] {"conf/comsumer.xml"});
+                new String[] {"config/consumer.xml"});
 
         DemoService demoService = (DemoService) factory.getBean("demoService");
         demoService.sayHello();
