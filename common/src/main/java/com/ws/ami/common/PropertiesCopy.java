f4aea6b1269a7890e61b67ab8aedab52e8741971
47206353@qq.com <47206353@qq.com>
2015-01-22 11:31:40 +0800

    类型转换

---
+++ common/src/main/java/com/ws/ami/common/PropertiesCopy.java
@@ -0,0 +1,45 @@
+package com.ws.ami.common;
+
+
+import org.apache.commons.beanutils.BeanUtils;
+
+/**
+ * 属性拷贝类：拷贝一个类的属性到另外一个目标类
+ * Created by hp on 2015/1/22.
+ */
+public class PropertiesCopy {
+
+    public static void main(String[] args) throws Exception {
+
+        spring();
+        apache();
+    }
+
+    /**
+     * spring bean BeanUtils的转换
+     */
+    private static void spring() {
+        Dog dog = new Dog();
+        dog.setDogMouth(1);
+        dog.setDogName("dogName");
+        dog.setAnimalMouth(1);
+        dog.setAnimalName("animalName");
+        TargetDog tdog = new TargetDog();
+       // BeanUtils.copyProperties(dog, tdog);
+    }
+
+
+    /**
+     * apache bean BeanUtils的转换
+     */
+    private static void apache() throws Exception
+    {
+        Dog dog = new Dog();
+        dog.setDogMouth(1);
+        dog.setDogName("dogName");
+        dog.setAnimalMouth(1);
+        dog.setAnimalName("animalName");
+        TargetDog tdog = new TargetDog();
+        BeanUtils.copyProperties(tdog,dog );
+    }
+}
